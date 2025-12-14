4c3b8f134baae45f84b4990dcc02cc1b48602c17
patel780 <34640822+patel780@users.noreply.github.com>
2025-12-14 19:03:18 +0530

    Add files via upload

---
+++ billgerator.py
@@ -0,0 +1,6 @@
+#write a program to find the bill divide in friends 
+bill=int(input("the bill is"))
+friends=int(input("the friends is"))
+pay=float(bill/friends)
+print("each will pay",pay,"\nand data type is",type(bill))
+print(type(friends))
---
+++ farehient.py
@@ -0,0 +1,5 @@
+celsius=float(input("ener the celsius"))
+celsius=(celsius*9/5)+32
+kelvin=celsius+273.15
+print("the fahreniet is this",celsius)
+print("the celsius is this",kelvin)
